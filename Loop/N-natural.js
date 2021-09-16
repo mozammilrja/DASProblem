8caab0db7a21c6631423ef362035b63282e014f8
mozammilrja <44997979+mozammilrja@users.noreply.github.com>
2021-09-16 09:12:26 +0530

    done

--- Loop/N-natural.js
+++ Loop/N-natural.js
@@ -7,3 +7,12 @@ for (let i = 0; i <= numbers; i++) {
 }
 
 document.write("sum of natural numbers: " + sum);
+// const numbers = parseInt(prompt("enter the natural numbers:"));
+
+// let sum = 0;
+
+// for (let i = 0; i <= numbers; i++) {
+//   sum += i;
+// }
+
+// document.write("sum of natural numbers: " + sum);
