1c39080a1098f785b81f6d44ee438802dd0ffffd
mozammil0218 <44997979+mozammilrja@users.noreply.github.com>
2023-07-01 22:10:46 +0530

    remove vowels

---
+++ Importent/index.js
@@ -0,0 +1,59 @@
+const employees = [
+  {
+    name: "John Smith",
+    salary: 50000,
+  },
+  {
+    name: "John Smith",
+    salary: 50000,
+  },
+  {
+    name: "Daniel Anderson",
+    salary: 55000,
+  },
+  {
+    name: "Emily Davis",
+    salary: 65000,
+  },
+  {
+    name: "David Wilson",
+    salary: 70000,
+  },
+  {
+    name: "Mozammil",
+    salary: 70000,
+  },
+  {
+    name: "Jennifer Taylor",
+    salary: 45000,
+  },
+];
+
+// const result = employees.reduce((a, b) => {
+//   if (a.salary > b.salary) {
+//     return a;
+//   } else if (a.salary < b.salary) {
+//     return b;
+//   } else {
+//     // If salaries are the same, prioritize based on name in alphabetical order
+//     return a.name.localeCompare(b.name) <= 0 ? a : b;
+//   }
+// });
+
+// console.log(result);
+
+// const arr = [2, 3, 4, 5, 6, 4, 3, 2];
+
+// const arr2 = [];
+
+// function duplicate(arr) {
+
+//     for (let i = 0; i < arr; i++) {
+//       for (let j = i + 1; j < arr; j++) {
+//         if (arr2[i] === arr2[j]) {
+//             arr2.push(j);
+//         }
+//         }
+//     }
+// }
+
--- JavaScriptBasics/Index.js
+++ JavaScriptBasics/Index.js
@@ -78,15 +78,272 @@
 
 // Find the longest word in a string
 
-function findLongestWord(str) {
-  let strSplit = str.split(" ");
-  let longestWord = "";
-  for (let i = 0; i < strSplit.length; i++) {
-    if (strSplit[i].length > longestWord.length) {
-      longestWord = strSplit[i];
-    }
-  }
-  return longestWord;
+// function findLongestWord(str) {
+//   let strSplit = str.split(" ");
+//   let longestWord = "";
+//   for (let i = 0; i < strSplit.length; i++) {
+//     if (strSplit[i].length > longestWord.length) {
+//       longestWord = strSplit[i];
+//     }
+//   }
+//   return longestWord;
+// }
+
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+
+// Find the intersection of two arrays.
+
+// const findIntersection = (arr, arr2) => {
+//   result = arr.filter((i) => arr2.includes(i));
+// };
+
+// const arr = [1, 2, 3, 4, 5];
+// const arr2 = [4, 5, 6, 7, 8];
+// const intersection = findIntersection(arr, arr2);
+// console.log(intersection); // Output: [4, 5]
+
+function findIntersection(arr1, arr2) {
+  const set2 = new Set(arr2);
+  return arr1.filter((value) => set2.has(value));
+  //   return arr1.filter((value) => arr2.includes(value));
 }
 
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+const arr = [1, 2, 3, 4, 5];
+const arr2 = [4, 5, 6, 7, 8];
+const intersection = findIntersection(arr, arr2);
+// console.log(intersection, "resutl"); // Output: [4, 5]
+
+// var isAnagram = (s, t) => {
+// return  s.split("").sort().join("") === t.split("").sort().join("");
+// };
+
+// console.log(isAnagram("anagram", "nagaram"));
+
+// const myArray = [1, 2, 3, 4, 5];
+
+// for (const element in myArray) {
+//   console.log(element,'in');
+// }
+
+// for (const element of myArray) {
+//   console.log(element,'of');
+// }
+
+// var isAnagram = (s, t) => {
+//   if (s.length !== t.length) return false;
+//   const map = new Map();
+//   for (const c of s) {
+//     if (map.has(c)) map.set(c, map.get(c) + 1);
+//     else map.set(c, 1);
+//   }
+
+//   for (const c of t) {
+//     if (!map.has(c)) return false;
+//     map.set(c, map.get(c) - 1);
+//       if (map.get(c) === 0) map.delete;
+//       if (map.get(c) === 0) map.delete(c);
+//   }
+
+//   if (map.size > 0) return false;
+
+//   return true;
+// };
+
+// console.log(isAnagram("anagram", "nagaram"));
+
+// function prime(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         const element = array[i];
+
+//     }
+// }
+// Find the factorial of a number.
+
+// let num = 2225;
+// let factorials = [];
+// let factorial = 1;
+// for (let i = 1; i <= num; i++) {
+//   factorial *= i;
+//   factorials.push(factorial);
+// }
+// console.log(factorial, "factorial");
+
+// const numbers = 120;
+// const res = 0;
+// for (let i = 5; i <= numbers; i = i * 5) {
+//     res + numbers/i;
+// }
+
+// console.log(res, "res");
+
+// start
+
+// const arr2 = [];
+
+// for (let i = 0; i < a.length; i++) {
+//   for (let j = i + 1; j < a.length; j++) {
+//     if (a[i] === a[j] && !arr2.includes(a[i])) {
+//       arr2.push(a[i]);
+//     }
+//   }
+// }
+
+// arr2.sort((a, b) => a - b);
+
+// Check if a number is prime
+function isPrime(num) {
+  if (num <= 1) {
+    return false; // Numbers less than or equal to 1 are not prime
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false; // Found a factor, not a prime number
+    }
+  }
+
+  return true; // No factors found, it is a prime number
+}
+
+const num = 37;
+// console.log(isPrime(num)); // Output: true
+
+// Remove a specific element from an array.
+
+// function swapNumber(a, b) {
+//   [a, b] = [b, a];
+//   return [a, b];
+// }
+
+// let x = 5;
+// let y = 10;
+
+// console.log(`swapNumber between ${x} and ${y}`);
+
+// [x, y] = swapNumber(x, y);
+
+// console.log(`swapNumber between x= ${x} and y= ${y}`);
+
+// function swapNumbers(a, b) {
+//   return [b, a];
+// }
+
+// // Example usage:
+// let x = 5;
+// let y = 10;
+// console.log(`Before swap: x = ${x}, y = ${y}`);
+
+// [x, y] = swapNumbers(x, y);
+// console.log(`After swap: x = ${x}, y = ${y}`);
+
+// Reverse the order of words in a string
+
+function reverseWords(str) {
+  let reversedString = "";
+  let word = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] !== " ") {
+      // If the character is not a space, prepend it to the word
+      word = str[i] + word;
+    } else {
+      // If the character is a space, add the word to the reversed string
+      reversedString += word + " ";
+      word = "";
+    }
+  }
+
+  // Add the last word to the reversed string
+  reversedString += word;
+
+  return reversedString;
+}
+
+// Example usage:
+// const sentence = "Hello, how are you?";
+// const reversedSentence = reverseWords(sentence);
+// console.log(reversedSentence);
+
+// function reverseWords(str) {
+//   const reversedString = str.split(" ").reverse().join(" ");
+//   return reversedString;
+// }
+// console.log(reverseWords("Hello, how are you?"));
+
+// Find the number of vowels in a string.
+// function findVowels(str) {
+//   if (
+//     str.includes("a") ||
+//     str.includes("e") ||
+//     str.includes("i") ||
+//     str.includes("o") ||
+//     str.includes("u")
+//   ) {
+//     return "vowels";
+//   } else {
+//     return "not in vowels";
+//   }
+// }
+
+// console.log(findVowels("hello world"));
+
+// function findVowels(str) {
+//   var vowels = ["a", "e", "i", "o", "u"];
+//   for (var i = 0; i < str.length; i++) {
+//     if (vowels.indexOf(str[i]) !== -1) {
+//       return "vowels";
+//     }
+//   }
+//   return "no vowels";
+// }
+
+// console.log(findVowels("hello world"));
+
+// function removeFalsyValues(arr) {
+//   return arr.filter(Boolean);
+
+// }
+
+// var array = [0, 1, false, true, "", "hello", null, undefined, NaN];
+
+// console.log(removeFalsyValues(array));
+
+// Calculate the average of numbers in an array
+
+// function avrageOfNumber(arr) {
+//   const sum = arr.reduce((a, b) => a + b, 0);
+//   var average = sum / arr.length;
+//   return average;
+// }
+
+// var numbers = [2, 4, 6, 8, 10];
+// var averageResult = avrageOfNumber(numbers);
+// console.log(averageResult);
+
+// Remove vowels from a string
+// function removeVowels(str) {
+//   return str.replace(/[aeiou]/gi, "");
+// }
+
+// var inputString = "Hello, World!";
+// var stringWithoutVowels = removeVowels(inputString);
+
+// console.log(stringWithoutVowels);
+
+function removeVowels(str) {
+  const vowels = new Set(["a", "e", "i", "o", "u"]);
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (!vowels.has(char)) {
+      result += char;
+    }
+  }
+
+  return result;
+}
+
+const words = "hello world";
+console.log(removeVowels(words));
