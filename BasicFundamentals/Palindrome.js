6cbb89a9b1d0d780eddee4d78c0382d0fb83f0df
mozammil0218 <44997979+mozammilrja@users.noreply.github.com>
2023-09-09 22:58:25 +0530

    added code palindrome and removeDuplicates

--- BasicFundamentals/Palindrome.js
+++ BasicFundamentals/Palindrome.js
@@ -3,18 +3,45 @@
 // 1331
 // 12321
 
-function Palindrome(x) {
-  if (x < 0) {
-    return false; // Negative numbers cannot be palindromes
-  }
-  // Convert the number to a string to work with its individual digits
-  let str = x.toString();
-  let len = str.length;
-  // Loop through the string from both ends towards the center
+// function Palindrome(x) {
+//   if (x < 0) {
+//     return false; // Negative numbers cannot be palindromes
+//   }
+//   // Convert the number to a string to work with its individual digits
+//   let str = x.toString();
+//   let len = str.length;
+//   // Loop through the string from both ends towards the center
+//   for (let i = 0; i < len / 2; i++) {
+//     // Compare characters at opposite positions in the string
+//     // If they are not the same, return false (not a palindrome)
+//     if (str[i] !== str[len - 1 - i]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// let num1 = 121;
+// let str2 = "racecar";
+// let num3 = -121;
+// let num4 = 12321;
+
+// console.log(Palindrome(num1)); // Output: true
+// console.log(Palindrome(str2)); // Output: true
+// console.log(Palindrome(num3)); // Output: false
+// console.log(Palindrome(num4)); // Output: true
+
+function isPalindrome(str) {
+  // Remove spaces and non-alphanumeric characters and convert to lowercase
+  const cleanStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  const len = cleanStr.length;
+
+  // Loop through the cleaned string from both ends towards the center
   for (let i = 0; i < len / 2; i++) {
-    // Compare characters at opposite positions in the string
+    // Compare characters at opposite positions in the cleaned string
     // If they are not the same, return false (not a palindrome)
-    if (str[i] !== str[len - 1 - i]) {
+    if (cleanStr[i] !== cleanStr[len - 1 - i]) {
       return false;
     }
   }
@@ -22,12 +49,8 @@ function Palindrome(x) {
   return true;
 }
 
-let num1 = 121;
-let str2 = "racecar";
-let num3 = -121;
-let num4 = 12321;
+let str = "A men is driving a car";
+let str2 = "A man, a plan, a canal, Panama";
 
-console.log(Palindrome(num1)); // Output: true
-console.log(Palindrome(str2)); // Output: true
-console.log(Palindrome(num3)); // Output: false
-console.log(Palindrome(num4)); // Output: true
+console.log(isPalindrome(str2)); // Output: true
+console.log(isPalindrome(str)); // Output: false
--- BasicFundamentals/RemoveDuplicates.js
+++ BasicFundamentals/RemoveDuplicates.js
@@ -1,9 +1,24 @@
-function RemoveDuplicateNumbers(num) {
-  const numStr = new Set(num);
-  return numStr;
-}
-let numStr = [
-  2, 3, 4, 9, 9, 9, 9, 5, 5, 5, 5, 6, 7, 8, 9, 9, 9, 9, 99, 10, 11, 12,
-];
+// function RemoveDuplicateNumbers(num) {
+//   const numStr = new Set(num);
+//   return numStr;
+// }
+// let numStr = [
+//   2, 3, 4, 9, 9, 9, 9, 5, 5, 5, 5, 6, 7, 8, 9, 9, 9, 9, 99, 10, 11, 12,
+// ];
 
-console.log(RemoveDuplicateNumbers(numStr));
+// console.log(RemoveDuplicateNumbers(numStr));
+
+function removeDuplicates(nums) {
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
+}
+
+const nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+console.log(removeDuplicates(nums2)); // Output: 5,
